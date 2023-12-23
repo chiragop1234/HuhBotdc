bd18c95420c16c8b365f33055d6d158e3ba9601b
Chirag sadhwani <138006300+chiragop1234@users.noreply.github.com>
2023-12-23 19:18:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,80 +1,93 @@
 import discord
 from discord.ext import commands
-import os
 from dotenv import load_dotenv
-import random
+import os
 
 load_dotenv()
 
-TOKEN = os.getenv('BOT_TOKEN')
-PREFIX = '!'
-OWNER_ID = 761886210120744990
-TOKEN_PER_MESSAGE = 0.2
-TOKEN_PER_INVITE = 10
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
-bot = commands.Bot(command_prefix=PREFIX)
-bot.remove_command('help')  # Remove the default help command
+intents = discord.Intents.default()
+intents.messages = True
 
-user_tokens = []
-credit_cards = []
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Load existing credit cards from a file
-with open('cc.txt', 'r') as file:
-    credit_cards = [line.strip() for line in file]
+# Dictionary to store user balances (user_id: tokens)
+user_balances = {}
+
+# Function to save user balances to a file
+def save_balances():
+    with open('balances.txt', 'w') as file:
+        for user_id, tokens in user_balances.items():
+            file.write(f'{user_id}:{tokens}\n')
+
+# Load user balances from a file (if the file exists)
+if os.path.exists('balances.txt'):
+    with open('balances.txt', 'r') as file:
+        for line in file:
+            user_id, tokens = line.strip().split(':')
+            user_balances[int(user_id)] = float(tokens)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
 
 @bot.event
 async def on_message(message):
-    await bot.process_commands(message)  # Process commands
+    if message.channel.id == CHANNEL_ID:
+        # Award tokens for messages
+        user_id = message.author.id
+        user_balances[user_id] = user_balances.get(user_id, 0) + 0.1
+        save_balances()
 
-    user_id = message.author.id
+    await bot.process_commands(message)
 
-    if user_id != bot.user.id:  # Ignore the bot's own messages
-        # Award tokens for sending messages
-        user_tokens.append(user_id)
+@bot.command()
+async def balance(ctx):
+    user_id = ctx.author.id
+    current_balance = user_balances.get(user_id, 0)
+    await ctx.send(f'Your current balance is {current_balance:.1f} tokens.')
+
+@bot.command()
+async def help(ctx):
+    help_message = (
+        "Welcome to the CC Bot!\n\n"
+        "**Commands:**\n"
+        "`!balance`: Check your token balance.\n"
+        "`!claim`: Claim a CC for 1 token.\n"
+        "`!help`: Display this help message."
+    )
+    await ctx.send(help_message)
+
+@bot.command()
+async def claim(ctx):
+    user_id = ctx.author.id
+    if user_balances.get(user_id, 0) >= 1:
+        user_balances[user_id] -= 1
+        save_balances()
+
+        # Implement CC claiming logic here
+        with open('cc.txt', 'r') as cc_file:
+            cc_content = cc_file.read()
+        cc_message = f"CLAIM YOUR FREE CC COST 1 TOKEN\n{cc_content}"
+        
+        # Create an interactive message with a Claim button
+        claim_button = discord.ui.Button(style=discord.ButtonStyle.green, label="Claim CC", custom_id="claim_cc")
+        view = discord.ui.View()
+        view.add_item(claim_button)
+
+        await ctx.send(cc_message, view=view)
+    else:
+        await ctx.send("Insufficient tokens. Earn more by chatting!")
 
 @bot.event
-async def on_invite_create(invite):
-    # Award tokens for creating an invite
-    user_id = invite.inviter.id
-    user_tokens.append(user_id)
-
-@bot.command(name='help')
-async def help_command(ctx):
-    embed = discord.Embed(
-        title='Bot Commands',
-        description='Use these commands to interact with the bot.',
-        color=discord.Color.blue()
-    )
-    embed.add_field(name=f'{PREFIX}balance', value='Check your token balance.', inline=False)
-    await ctx.send(embed=embed)
-
-@bot.command(name='balance')
-async def balance_command(ctx, member: discord.Member = None):
-    member = member or ctx.author
-    balance = user_tokens.count(member.id) * TOKEN_PER_MESSAGE
-    await ctx.send(f'{member.mention}, your token balance: {balance}')
-
-@bot.command(name='claim')
-async def claim_command(ctx):
-    user_id = ctx.author.id
-
-    if user_id == OWNER_ID:
-        user_tokens.append(user_id)  # Owner has unlimited tokens
-    else:
-        if user_tokens.count(user_id) >= 1 and credit_cards:
-            # Get and remove a credit card from the list
-            cc_details = credit_cards.pop()
-
-            # Send a DM with the credit card details
-            await ctx.author.send(f'Claim your free credit card!\n\n{cc_details}')
-        else:
-            await ctx.send("Sorry, you don't have enough tokens or there are no more credit cards.")
-
-    # Send a message in the channel indicating the DM
-    await ctx.send("Check your DMs for the credit card details!")
+async def on_button_click(interaction):
+    if interaction.custom_id == "claim_cc":
+        # Implement additional logic for processing CC claims
+        # For example, you can read from cc.txt and send the CC
+        with open('cc.txt', 'r') as cc_file:
+            cc_content = cc_file.read()
+        await interaction.response.send_message(f"CC claimed successfully!\n{cc_content}", ephemeral=True)
 
 bot.run(TOKEN)
